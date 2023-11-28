f33e045f37674a3821ed264d1b448007060b4220
ivo-gla <150912335+ivo-gla@users.noreply.github.com>
2023-11-28 12:01:55 +0000

    added chi stuff

--- Exercises2023/Ex1_2/CustomFunctions.cxx
+++ Exercises2023/Ex1_2/CustomFunctions.cxx
@@ -172,6 +172,69 @@ std::string LeastSquaresCalc(std::vector<std::vector<float>> coordinates){
 
 
 
+    return equation;
+}
+
+
+
+std::string LeastSquaresCalc_wCHI(std::vector<std::vector<float>> coordinates){
+
+    std::string equation;
+    std::vector<std::vector<float>> errors = readLines("error2D_float.txt");
+    double p;
+    double q;
+    double sum_x;
+    double sum_y;
+    double sum_xy;
+    double sum_xx;
+    double N; 
+    double error_x;
+    double error_y;
+
+    //std::cout << "COORDS SIZE" <<coordinates.size() << std::endl;
+    N = coordinates.size();
+
+    for (int i = 0; i <= N; ++i){
+        error_x = errors[i][0];
+        error_y = errors[i][1];
+
+        sum_x += coordinates[i][0];
+        sum_y += coordinates[i][1];
+        sum_xy += coordinates[i][0] * coordinates[i][1];
+        sum_xx += coordinates[i][0] * coordinates[i][0];
+        
+        
+        //std::cout << row[0] << " " <<row[1] << std::endl;
+        //std::cout << row[1] << std::endl;
+        //float mag1 = magnitude(row);
+        //std::cout << "The Magnitude of this vector: (" << row[0] << " " <<row[1] << "), is: "<<mag1 << std::endl;
+        
+    }
+
+    p = (N * sum_xy - sum_x * sum_y) / (N * sum_xx - sum_x * sum_x);
+    q = (sum_xx * sum_y - sum_xy * sum_x) / (N * sum_xx - sum_x * sum_x);
+
+    std::cout << "p" << p << std::endl;
+    std::cout << "q" << q << std::endl;
+
+    /*
+    $$
+    y=p x+q 
+    p=\frac{N \sum x_i y_i-\sum x_i \sum y_i}{N \sum x_i^2-\sum x_i \sum x_i},
+    q=\frac{\sum x_i^2 \sum y_i-\sum x_i y_i \sum x_i}{N \sum x_i^2-\sum x_i \sum x_i}
+    $$
+    */
+    std::string p1 = "The equation is y = ";
+    std::string pString = std::to_string(p);
+    std::string p2 = "x +";
+    std::string qString = std::to_string(q);
+    equation = p1.append(pString).append(p2).append(qString);
+
+
+
+    //equation = "The equation is y = " << p <<"x + "<< q <<;
+
+
 
 
 
