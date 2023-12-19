4d14f98f2b2d67797097cdf40bdc0b5d20cc2659
ivo-gla <150912335+ivo-gla@users.noreply.github.com>
2023-12-19 11:40:19 +0000

    added functions to implement

---
+++ Exercises2023/Ex3_4/new_functions.cxx
@@ -0,0 +1,41 @@
+#include "FiniteFunctions.h"
+#include <string>
+#include <vector>
+#include "gnuplot-iostream.h"
+#include <cmath>
+
+
+class normal_distrobution : public FiniteFunction {
+
+public:
+    normal_distrobution();
+
+//protected:
+
+private:
+
+};
+
+
+class cauchy_lorentz : public FiniteFunction {
+
+public:
+    cauchy_lorentz();
+
+//protected:
+
+private:
+    
+};
+
+
+class crystal_ball : public FiniteFunction {
+
+public:
+    crystal_ball();
+
+//protected:
+
+private:
+
+};
